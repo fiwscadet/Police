4a95a5e0b293fa359da2436e00ae6e25a7def760
fiwscadet <50857528+fiwscadet@users.noreply.github.com>
2019-10-27 04:51:23 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,158 @@
+'use strict'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+var request = require('request');
+const app = express();
+var encoding = require("encoding");
+
+
+
+app.set('port', (process.env.PORT || 5000)) 
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
+var Token = process.env.TOKEN;
+//var WebDF = process.env.WebDF;
+
+var access_token = 'Bearer {'+Token+'}'
+var name = "";
+
+app.get('/', (req, res) => {
+  res.end("ok")
+})
+
+
+
+app.post('/webhook', (req, res) => {
+
+    var data = req.body; 
+console.log('-----ปริ้นทั้งหมด-----' + JSON.stringify(data));
+    
+//console.log('++++'+JSON.stringify(req.body.events));
+    
+    var sende_r = req.body.events[0].source.userId
+    var reToken = req.body.events[0].replyToken
+    
+    var data = req.body; 
+    
+    if (req.body.events[0].type == "message") { 
+    if (req.body.events[0].message.type == "text") {
+     
+     var tex_t = req.body.events[0].message.text
+     
+        //profile(reToken, sende_r); 
+
+     }
+        
+     }else if (req.body.events[0].type == "beacon") {  
+     if (req.body.events[0].beacon.type == "enter") {   
+        
+     var beacon_id = req.body.events[0].beacon.hwid  
+       //  if(beacon_1 == "0133305fbd"){
+       //  profile(reToken, sende_r);    
+       //  }else if(beacon_1 == "013332298c"){
+       //  profile(reToken, sende_r);    
+       //  } 
+       profile(reToken, sende_r, beacon_id);  
+       console.log("---enter---" + sende_r+"-------"+beacon_id);  
+     }
+ }
+ 
+ 
+ })
+ 
+ 
+ /////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+function data_enter(sende_r, beacon_id) {
+
+
+   var checktime = "https://script.google.com/macros/s/AKfycbwaKkPZI8T248QXVlGGB-1N9et9rpw54ZshUuFboXY5J5zxTt0/exec?userId="+sende_r+"&beaconid="+beacon_id;
+ request(checktime, function (error, response, body) {
+
+})
+
+
+}
+
+
+
+
+
+// เช็คโปรไฟล์
+/////////////////////////////////////////////////////////////////////////
+
+function profile(reToken,sende_r, beacon_id) {
+
+    var headers = {
+       'Authorization' : access_token
+}
+    
+
+var options = {
+    url: 'https://api.line.me/v2/bot/profile/'+sende_r,
+    method: 'GET',
+    headers: headers,
+    }
+
+ request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var info = JSON.parse(body);
+        name = info.displayName;
+        
+          if(beacon_id == "0133305fbd"){
+         var msg = {
+           "type": "text",
+           "text": "ระบบได้บันทึกข้อมูลของคุณ "+ name + "\nเรียบร้อยแล้ว\n Beacon 1"
+          }; 
+        reply(reToken, msg, sende_r, beacon_id);    
+         }else if(beacon_id == "013332298c"){
+         var msg = {
+           "type": "text",
+           "text": "ระบบได้บันทึกข้อมูลของคุณ "+ name + "\nเรียบร้อยแล้ว\n Beacon 2"
+          }; 
+        reply(reToken, msg, sende_r, beacon_id);   
+         }
+         
+        
+        
+        
+        
+    }
+ })
+
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+function reply(reToken, msg, sende_r, beacon_id) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': access_token
+    }
+    let body = JSON.stringify({
+        replyToken: reToken,
+        messages: [msg]
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }
+)
+    console.log("---data_enter---" + sende_r+"-------"+beacon_id);
+data_enter(sende_r, beacon_id);
+}
+
+
+
+
